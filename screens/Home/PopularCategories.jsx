a27c76301df7ee5021c5887d06021e0fcf4b6019
pomff <105871459+pomfuu@users.noreply.github.com>
2024-10-05 22:36:32 +0700

    10/05

---
+++ screens/Home/PopularCategories.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PopularCategories = () => {
+  return (
+    <View>
+      <Text>PopularCategories</Text>
+    </View>
+  )
+}
+
+export default PopularCategories
+
+const styles = StyleSheet.create({})
