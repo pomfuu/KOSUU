ca43165c8db0b162a1cab2f274da9e9e3a31d342
pomff <105871459+pomfuu@users.noreply.github.com>
2024-10-07 17:29:20 +0700

    test

---
+++ screens/Order/NoOrder.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NoOrder = () => {
+  return (
+    <View>
+      <Text>NoOrder</Text>
+    </View>
+  )
+}
+
+export default NoOrder
+
+const styles = StyleSheet.create({})
---
+++ screens/Order/OrderCompleted.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const OrderCompleted = () => {
+  return (
+    <View>
+      <Text>OrderCompleted</Text>
+    </View>
+  )
+}
+
+export default OrderCompleted
+
+const styles = StyleSheet.create({})
