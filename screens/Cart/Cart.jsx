a82e2d5b8e6100f1cb1a4eb8974cc2ed8713cfed
kangnul <135142373+kangnul@users.noreply.github.com>
2024-12-04 09:28:52 +0700

    Tes
    Tes

--- screens/Cart/Cart.jsx
+++ screens/Cart/Cart.jsx
@@ -17,7 +17,7 @@ const Cart = () => {
 
   const fetchCartItems = async (userId) => {
     try {
-      // Nyari data Cart dari user yg sekarang log in
+      // Reference to the user's cart subcollection-
       const cartRef = collection(db, 'Users', userId, 'Cart');
       const q = query(cartRef); 
       const querySnapshot = await getDocs(q);
